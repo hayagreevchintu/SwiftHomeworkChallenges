6cc7ff86a08eaa64ae50a4a923d297999c8a5e7e
Hayagreev Varadarajan <hayagreev.chintu1@gmail.com>
2021-05-30 18:20:49 +0530

    solved challenge 7

---
+++ challenge7.swift
@@ -0,0 +1,30 @@
+/*
+
+Question:
+Create a function called walkNorth and have it print the line “You walked North”.
+Then, create a function called walkSouth and have it print the line “You walked South”.
+Then, create a function called walkEast and have it print the line “You walked East”.
+Then, create a function called walkWest and have it print the line “You walked West”.
+Finally, call all the functions you have created.
+
+*/
+func walkNorth(){
+    print("You walked North")
+}
+
+func walkSouth(){
+    print("You walked South")
+}
+
+func walkEast(){
+    print("You walked East")
+}
+
+func walkWest(){
+    print("You walked West")
+}
+
+walkNorth()
+walkSouth()
+walkEast()
+walkWest()
